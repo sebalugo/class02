01c96b6c9e8992ccf84e38ff1aaadc5cc7dced78
root <root@ubuntu.(none)>
2014-05-27 10:46:05 -0700

    Team add player correct instance

--- team_spec.rb
+++ team_spec.rb
@@ -1,3 +1,6 @@
+require "player.rb"
+require "spec_helper.rb"
+
 class Team
   
   attr_reader :team_name
@@ -5,16 +8,16 @@ class Team
   def initialize (team_name) 
 
   @team_name = team_name
-  team = []
+  @team = []
   end
 
-  def add_player(name, position)
-   Player player = Player.new name,postion
-   team.push(player)
+  def add_player(name,position)
+   player = Player.new name,position
+   @team.push(player)
   end
 
   def remove_player(name)
-    team.delete_if { |a_player| a_player.name == name} 
+    @team.delete_if { |a_player| a_player.name == name} 
   end
 
   def filter_by_position(position)
@@ -22,6 +25,8 @@ class Team
   end
 
   def to_s
- # output team name + players list.
+ 
+  # output team name + players list.
   end
+
 end
